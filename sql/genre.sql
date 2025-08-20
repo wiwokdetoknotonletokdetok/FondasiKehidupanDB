b89d5136023b80e668cfb01532a6a13d6be37ef5
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-08-20 22:48:59 +0700

    genre enum

---
+++ sql/genre.sql
@@ -0,0 +1,20 @@
+INSERT INTO GENRE (id, genre) VALUES
+(1, 'Roman / Cinta'),
+(2, 'Fantasi'),
+(3, 'Fiksi Ilmiah'),
+(4, 'Horor'),
+(5, 'Misteri / Detektif'),
+(6, 'Thriller / Ketegangan'),
+(7, 'Drama'),
+(8, 'Fiksi Sejarah'),
+(9, 'Perjalanan Hidup');
+
+INSERT INTO GENRE (id, genre) VALUES
+(10, 'Biografi / Autobiografi'),
+(11, 'Pengembangan Diri / Self-help'),
+(12, 'Bisnis & Kepemimpinan'),
+(13, 'Sains / Ilmu Pengetahuan'),
+(14, 'Sejarah'),
+(15, 'Filsafat'),
+(16, 'Agama / Spiritual'),
+(17, 'Pendidikan / Referensi');
